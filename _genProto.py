3e2a2f2847eef3369cf9569abcd89bf46397b7c0
carbon <carbon@mac17.lan>
2018-07-06 00:32:35 +0800

    init

--- _genProto.py
+++ _genProto.py
@@ -13,17 +13,22 @@ PROTO_PATH = joinDir(ROOT_PATH, "proto2-lua", "proto")
 os.chdir(PROTO_PATH)
 
 # lua的proto信息生成
-if os.path.exists("../lua") == False:
-    os.mkdir("../lua")
+if os.path.exists("../lua"):
+    shutil.rmtree("../lua")
+os.mkdir("../lua")
 os.system("../protoc2 --plugin=protoc-gen-lua='../plugin/protoc-gen-lua' --lua_out=../lua *.proto")
 
 # go的proto信息生成
-if os.path.exists("../go") == False:
-    os.mkdir("../go")
-# proto文件生成
-os.system("../protoc3 --plugin=protoc-gen-gogofaster=../protoc-gen-gogofaster --gogofaster_out=../go *.proto")
-# 消息id生成
-os.system("../protoc3 --plugin=protoc-gen-msg=../protoc-gen-msg --msg_out=../go/msg_id.go:. *.proto")
+if os.path.exists("../go"):
+    shutil.rmtree("../go")
+os.mkdir("../go")
+for file in os.listdir(PROTO_PATH):
+    if file.endswith(".proto"):
+        prefix = file.split(".")[0]
+        # proto文件生成
+        os.system("../protoc3 --plugin=protoc-gen-gogofaster=../protoc-gen-gogofaster --gogofaster_out=../go {}".format(file))
+        # 消息id生成
+        os.system("../protoc3 --plugin=protoc-gen-msg=../protoc-gen-msg --msg_out=../go/{}-msg.go:. {}".format(prefix, file))
 
 #复制proto文件
 for file in os.listdir(PROTO_PATH):
@@ -35,9 +40,9 @@ for file in os.listdir(PROTO_PATH):
 LUA_PROTO_PATH = joinDir(ROOT_PATH, "gobang-client", "src", "app", "proto")
 if os.path.exists(LUA_PROTO_PATH):
     shutil.rmtree(LUA_PROTO_PATH)
-shutil.move("../lua", LUA_PROTO_PATH)
+shutil.copytree("../lua", LUA_PROTO_PATH)
 
 GO_PROTO_PATH = joinDir(ROOT_PATH, "gobang-server", "proto")
 if os.path.exists(GO_PROTO_PATH):
     shutil.rmtree(GO_PROTO_PATH)
-shutil.move("../go", GO_PROTO_PATH)
+shutil.copytree("../go", GO_PROTO_PATH)
